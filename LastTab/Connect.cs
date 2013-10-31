27ebbaa5ad62a99f8ade5e363a86c6b4c17b548e
Michael L Perry <michael@qedcode.com>
2013-10-31 10:43:32 -0500

    Initial commit.

---
+++ LastTab/Connect.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Extensibility;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.CommandBars;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+using Microsoft.Win32;
+using System.Linq;
+
+namespace LastTab
+{
+	/// <summary>The object for implementing an Add-in.</summary>
+	/// <seealso class='IDTExtensibility2' />
+	public class Connect : IDTExtensibility2, IDTCommandTarget
+	{
+		/// <summary>Implements the constructor for the Add-in object. Place your initialization code within this method.</summary>
+		public Connect()
+		{
+		}
+
+		/// <summary>Implements the OnConnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is being loaded.</summary>
+		/// <param term='application'>Root object of the host application.</param>
+		/// <param term='connectMode'>Describes how the Add-in is being loaded.</param>
+		/// <param term='addInInst'>Object representing this Add-in.</param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+		{
+			_applicationObject = (DTE2)application;
+			_addInInstance = (AddIn)addInInst;
+			if(connectMode == ext_ConnectMode.ext_cm_UISetup)
+			{
+				object []contextGUIDS = new object[] { };
+				Commands2 commands = (Commands2)_applicationObject.Commands;
+
+				//This try/catch block can be duplicated if you wish to add multiple commands to be handled by your Add-in,
+				//  just make sure you also update the QueryStatus/Exec method to include the new command names.
+				try
+				{
+					//Add a command to the Commands collection:
+					Command command = commands.AddNamedCommand2(_addInInstance, "LastTab", "Reopen closed tab", "Opens the file that was most recently closed", true, 59, ref contextGUIDS, (int)vsCommandStatus.vsCommandStatusSupported+(int)vsCommandStatus.vsCommandStatusEnabled, (int)vsCommandStyle.vsCommandStylePictAndText, vsCommandControlType.vsCommandControlTypeButton);
+
+                    if (command != null)
+                    {
+                        command.Bindings = new object[2]
+                        {
+                            "Global::Ctrl+Shift+T",
+                            "Text Editor::Ctrl+Shift+T"
+                        };
+                    }
+				}
+				catch(System.ArgumentException)
+				{
+					//If we are here, then the exception is probably because a command with that name
+					//  already exists. If so there is no need to recreate the command and we can 
+                    //  safely ignore the exception.
+				}
+			}
+		}
+
+		/// <summary>Implements the OnDisconnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is being unloaded.</summary>
+		/// <param term='disconnectMode'>Describes how the Add-in is being unloaded.</param>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnDisconnection(ext_DisconnectMode disconnectMode, ref Array custom)
+		{
+		}
+
+		/// <summary>Implements the OnAddInsUpdate method of the IDTExtensibility2 interface. Receives notification when the collection of Add-ins has changed.</summary>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />		
+		public void OnAddInsUpdate(ref Array custom)
+		{
+		}
+
+		/// <summary>Implements the OnStartupComplete method of the IDTExtensibility2 interface. Receives notification that the host application has completed loading.</summary>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnStartupComplete(ref Array custom)
+		{
+		}
+
+		/// <summary>Implements the OnBeginShutdown method of the IDTExtensibility2 interface. Receives notification that the host application is being unloaded.</summary>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnBeginShutdown(ref Array custom)
+		{
+		}
+		
+		/// <summary>Implements the QueryStatus method of the IDTCommandTarget interface. This is called when the command's availability is updated</summary>
+		/// <param term='commandName'>The name of the command to determine state for.</param>
+		/// <param term='neededText'>Text that is needed for the command.</param>
+		/// <param term='status'>The state of the command in the user interface.</param>
+		/// <param term='commandText'>Text requested by the neededText parameter.</param>
+		/// <seealso class='Exec' />
+		public void QueryStatus(string commandName, vsCommandStatusTextWanted neededText, ref vsCommandStatus status, ref object commandText)
+		{
+			if(neededText == vsCommandStatusTextWanted.vsCommandStatusTextWantedNone)
+			{
+				if(commandName == "LastTab.Connect.LastTab")
+				{
+					status = (vsCommandStatus)vsCommandStatus.vsCommandStatusSupported|vsCommandStatus.vsCommandStatusEnabled;
+					return;
+				}
+			}
+		}
+
+		/// <summary>Implements the Exec method of the IDTCommandTarget interface. This is called when the command is invoked.</summary>
+		/// <param term='commandName'>The name of the command to execute.</param>
+		/// <param term='executeOption'>Describes how the command should be run.</param>
+		/// <param term='varIn'>Parameters passed from the caller to the command handler.</param>
+		/// <param term='varOut'>Parameters passed from the command handler to the caller.</param>
+		/// <param term='handled'>Informs the caller if the command was handled or not.</param>
+		/// <seealso class='Exec' />
+        public void Exec(string commandName, vsCommandExecOption executeOption, ref object varIn, ref object varOut, ref bool handled)
+        {
+            handled = false;
+            if (executeOption == vsCommandExecOption.vsCommandExecOptionDoDefault)
+            {
+                if (commandName == "LastTab.Connect.LastTab")
+                {
+                    string lastFileClosed = GetLastFileClosed();
+                    if (lastFileClosed != null)
+                        _applicationObject.DTE.ItemOperations.OpenFile(lastFileClosed);
+
+                    handled = true;
+                    return;
+                }
+            }
+        }
+		private DTE2 _applicationObject;
+		private AddIn _addInInstance;
+
+        private string GetLastFileClosed()
+        {
+            string version = _applicationObject.Version;
+            using (var key = Registry.CurrentUser.OpenSubKey(
+                String.Format(@"Software\Microsoft\VisualStudio\{0}\FileMRUList", version)))
+            {
+                int index = 1;
+                while (true)
+                {
+                    var value = key.GetValue(String.Format("File{0}", index)) as string;
+                    if (value == null)
+                        return null;
+
+                    var segments = value.Split('|');
+                    if (segments.Length > 0)
+                    {
+                        var fileName = segments[0];
+                        if (!_applicationObject.DTE.ItemOperations.IsFileOpen(fileName))
+                            return fileName;
+                    }
+                    index++;
+                }
+            }
+        }
+    }
+}
